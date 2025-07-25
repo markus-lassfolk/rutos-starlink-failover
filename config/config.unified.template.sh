56111f967818423b13d7b70f9b5952a4212dd536
GitHub Action <action@github.com>
2025-07-25 13:16:34 +0000

    🎨 Auto-format shell scripts with shfmt [skip ci]

--- config/config.unified.template.sh
+++ config/config.unified.template.sh
@@ -21,7 +21,7 @@
 
 # Version information (auto-updated by update-version.sh)
 if [ -z "${SCRIPT_VERSION:-}" ]; then
-SCRIPT_VERSION="2.7.0"
+    SCRIPT_VERSION="2.7.0"
     readonly SCRIPT_VERSION
 fi
 
